719fe4715911a0544c45e97ee684eaf382626996
laribox <laribox2014@gmail.com>
2023-10-25 00:22:09 +0100

    task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,23 +1,34 @@
 #include "sort.h"
 /**
-* selection_sort - slow alg
-* @array: input array
-* @size: size of the input
-*/
+ * selection_sort - sorts the array by selection algorithm
+ * @array: the pointer to the array
+ * @size: the size of the array
+ * Return: void
+ */
 void selection_sort(int *array, size_t size)
 {
-size_t i, j, low, tmp;
-int *head;
+	size_t i, j, min_idx;
+	int k;
 
-head = array;
-for (i = 0; i < size; i++, head++)
-{
-for (low = i, j = i; j < size; j++)
-if (array[j] < array[low])
-low = j;
- tmp = array[low];
-array[low] = *head;
-*head = tmp;
-print_array(array, size);
-}
+	if (array == NULL || size < 2)
+		return;
+	for (i = 0; i < size - 1; i++)
+	{
+		min_idx = i;
+
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_idx])
+				min_idx = j;
+		}
+
+		if (min_idx != i)
+		{
+			k = array[min_idx];
+			array[min_idx] = array[i];
+			array[i] = k;
+
+			print_array(array, size);
+		}
+	}
 }
