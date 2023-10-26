b815c7c60b648661c924f7e6d9579b345c300fe9
laribox <laribox2014@gmail.com>
2023-10-26 09:34:21 +0100

    fix

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,7 +1,7 @@
 #include "sort.h"
 
 /**
-* swap - Swap two integers in an array.
+* swap_ints - Swap two integers in an array.
 * @a: The first integer to swap.
 * @b: The second integer to swap.
 */
